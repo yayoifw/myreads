2e19a64aabebc4a3e966a885772475c55b1613f9
yayoi <yayoi@forwardlogictech.com>
2017-09-13 22:17:23 -0400

    SearchBookScreen and added react-router-dom to navigate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ListBooksScreen from './ListBooksScreen'
+import SearchBookScreen from './SearchBookScreen'
+import { Route } from 'react-router-dom'
 
 
 
@@ -7,7 +9,8 @@ class App extends Component {
   render() {
     return (
 <div className="App">
-<ListBooksScreen />
+<Route exact path="/" render={() => (<ListBooksScreen />)} />
+<Route path="/search" render={() => (<SearchBookScreen />)} />
 </div>
     );
   }
