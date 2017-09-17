69afe9a6070f0353795e168c57a6d3da87afbd10
yayoi <yayoi@forwardlogictech.com>
2017-09-16 22:38:24 -0400

    Add state.shelf in Book component

--- src/Book.js
+++ src/Book.js
@@ -8,6 +8,10 @@ class Book extends Component {
     handleBookMove: PropTypes.func.isRequired
   }
 
+  state = {
+    shelf: "none"
+  }
+
   constructor() {
     super()
     this.handleChange = this.handleChange.bind(this)
@@ -15,6 +19,7 @@ class Book extends Component {
 
   handleChange(event) {
     const selectedShelf = event.target.value;
+    this.setState({ shelf: selectedShelf })
     this.props.handleBookMove(selectedShelf, this.props.book)
   }
 
@@ -26,7 +31,7 @@ class Book extends Component {
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: bookImage }}></div>
             <div className="book-shelf-changer">
-              <select value={(typeof book.shelf === "undefined") ? "none" : book.shelf} onChange={this.handleChange}>
+              <select value={(typeof book.shelf !== "undefined") ? book.shelf : this.state.shelf } onChange={this.handleChange}>
                 <option value="none" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
